5cfc978ff826b9ecf88073b88ec0054f72495bfe
ItsSiddharth <siddharth.menon1@gmail.com>
2020-01-12 19:48:04 +0530

    Updated Code

--- prediction.py
+++ prediction.py
@@ -8,7 +8,7 @@ HEIGHT = 256
 LR = 0.01
 MODEL_NAME = "cyclist-CNN-4.model"
 
-image = cv2.imread("tsinghuaDaimlerDataset_2014-11-20_074640_000000010_leftImg8bit.png")
+image = cv2.imread("test_img.png")
 
 
 
@@ -18,12 +18,13 @@ image1 = np.array(image1).reshape(-1, 256, 256, 1)
 image1 = image1.astype(np.float32)
 image1 = image1/255.0
 prediction = model.predict(image1)
-prediction[0][2]*=4
-prediction[0][0]*=4
-prediction[0][1]*=8
-prediction[0][3]*=8
+prediction[0][2]*=2
+prediction[0][0]*=2
+prediction[0][1]*=2
+prediction[0][3]*=2
 print(prediction[0])
 label = prediction[0]
+image = cv2.resize(image, (512,512))
 cv2.rectangle(image, (label[1], label[0]), (label[3], label[2]), (255,0,0), 2)
 cv2.imshow('frame', image)
 cv2.waitKey(0)
